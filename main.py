bb6532e651c82640b7a777fe4ecb7af49e3ee5f4
drowlink <pauloriveiro01@gmail.com>
2023-10-02 13:28:37 -0400

    creating new routes with parameters and queries

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/calculator")
+def calcular(valor1: float, valor2: float):
+    return {"suma": valor1 + valor2}
