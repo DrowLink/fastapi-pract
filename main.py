e41c770226a71890cd4605529aa58bb4cf7f8665
drowlink <pauloriveiro01@gmail.com>
2023-10-02 14:02:00 -0400

    models separated in its folder

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from typing import Union
 from fastapi import FastAPI
-from pydantic import BaseModel
+from models.item_model import Item
 
 app = FastAPI()
+
 
 
 @app.get("/")
@@ -18,3 +19,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get("/calculator")
 def calcular(valor1: float, valor2: float):
     return {"suma": valor1 + valor2}
+
+@app.put('/items/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
